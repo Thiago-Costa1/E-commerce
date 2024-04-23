20e6a1cf30fd4936b306471b6ee7ab2ca0042467
Thiago <thiagocostas2017@gmail.com>
2024-04-23 17:42:23 -0300

    criado a seção de opinião dos clientes

---
+++ java.js
@@ -0,0 +1,11 @@
+function mostrarPergunta(id)
+ {
+    var resposta = document.getElementById(id);
+    resposta.style.display = "block";
+}
+
+function esconderPergunta(id)
+{
+    var resposta = document.getElementById(id);
+    resposta.style.display = "none";
+}
