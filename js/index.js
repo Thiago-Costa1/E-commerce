50c6fa8388dd75990324ee27bb07dff32714ae71
Thiago <thiagocostas2017@gmail.com>
2024-05-06 18:26:50 -0300

    carrossel adicionado

--- js/index.js
+++ js/index.js
@@ -11,3 +11,28 @@ function esconderPergunta(id)
 
 }
 
+ // Início do javascript do carrossel //
+
+    var radio = document.querySelector('.manual-btn')
+	var cont = 1
+
+	document.getElementById('radio1').checked = true 	
+
+	setInterval(() =>
+    {
+		proximaImg()
+	}, 5000)
+
+	function proximaImg()
+    {
+		cont++
+		if(cont > 3){
+			cont = 1
+		}
+
+	// Marca o botão de rádio correspondente à próxima imagem como selecionado
+		document.getElementById('radio'+cont).checked = true 
+
+	}
+
+ // Fim do javascript do carrossel //
