acb5877d70f6b2eafb59044b48a01c44d9ef4921
Thiago <thiagocostas2017@gmail.com>
2024-05-09 16:46:31 -0300

    alteração na estilização do modal

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
-function mostrarPergunta(id)
-{
-    var resposta = document.getElementById(id);
-    resposta.style.display = "block";
-}
-
-function esconderPergunta(id)
-{
-    var resposta = document.getElementById(id);
-    resposta.style.display = "none";
-
-}
-
 // Início do javascript do carrossel //
 
 	var radio = document.querySelector('.manual-btn')
@@ -43,39 +30,59 @@ function formatarTelefone(input)
 	var numero = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
 
 	if (numero.length >= 2)
-	numero = '(' + numero.substring(0, 2) + ')' + numero.substring(2);
+	    numero = '(' + numero.substring(0, 2) + ')' + numero.substring(2);
 	if (numero.length >= 8)
-	  numero = numero.substring(0, 8) + '-' + numero.substring(8);
+	    numero = numero.substring(0, 8) + '-' + numero.substring(8);
 
 	input.value = numero;  // Atualizar o valor do input
 }
 	
 // Fim da formatação do número do usuário no model
 
-// Início da formatação do X para abrir e fechar o modal
+// Início da formatação do "X" para abrir e fechar o modal
 
-function fecharModal(event) {
-    var modal = document.querySelector('.container_modal');
-    var fundoEscuro = document.getElementById('fundo_escuro');
-    // Verificar se o clique ocorreu fora do modal
-    if (event.target === fundoEscuro) {
-        modal.style.display = 'none';
-        fundoEscuro.style.display = 'none';
-    }
-}
-
-function exibirModal() {
-    var modal = document.querySelector('.container_modal');
+function exibirModal()
+{
+    var modal = document.querySelector('.container_modal', );
     var fundoEscuro = document.getElementById('fundo_escuro');
     modal.style.display = 'block';
     fundoEscuro.style.display = 'block';
 }
 
-function impedirPropagacao(event) {
-    // Impedir que o evento seja propagado para o fundo escuro
-    event.stopPropagation();
+function fecharModal(event)
+{
+    var modal = document.querySelector('.container_modal');
+    var fundoEscuro = document.getElementById('fundo_escuro');
+  
+    event.preventDefault();   // Evitar o comportamento padrão do botão ir para o início da página
+  
+    if (event.target.classList.contains('fechar_modal'))
+    {
+        modal.style.display = 'none';
+        fundoEscuro.style.display = 'none';
+    }
 }
+
+
+
 
 
 // Fim da formatação do X para fechar o modal
 
+
+// Início da formatação para mostrar e esconder as respostas
+
+function mostrarPergunta(id)
+{
+    var resposta = document.getElementById(id);
+    resposta.style.display = "block";
+}
+
+function esconderPergunta(id)
+{
+    var resposta = document.getElementById(id);
+    resposta.style.display = "none";
+
+}
+
+// Fim da formatação para mostrar e esconder as respostas
